2dd27d391141dd0d10c4e4812c077679c4f2f6e9
aliiii786 <alirizviiii2024@gmail.com>
2026-07-27 22:09:14 +0530

    add pivot ques

---
+++ Arrays/pivotelement.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int pivotElement(int arr[],int size){
+    int start = 0 ;
+     int end = size - 1;
+     int mid = start + (end - start)/2;
+
+     while(start<=end){
+        if(arr[mid]>=arr[0]){
+            start = mid +1;
+        }
+        else{
+            end = mid;
+        }
+        mid = start + (end - start)/2;
+     }
+     return start;
+
+
+}
+
+int main(){
+    int arr[] = {3,8,10,17,1};
+    int ans = pivotElement(arr,5);
+    cout<<"ans is "<<ans;
+}
