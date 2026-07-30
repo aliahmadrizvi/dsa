d60fd55461c8166cd37c26163054e9154a4805dc
Ali Ahmad Rizvi <alirizviiii2024@gmail.com>
2026-07-30 21:48:11 +0530

    rotated sorted ques done

---
+++ Search/rotatedSortedques.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+
+int getPivot(int arr[],int size){
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end - start)/2;
+
+    while(start<=end){
+        if(arr[mid]>=arr[0]){
+            start = mid + 1;
+        }
+        else{
+            end =mid;
+        }
+        mid = start + (end-start)/2;
+    }
+    return start;
+
+}
+
+int BinarySearch(int arr[],int s,int e,int key){
+    int start = s;
+    int end = e;
+    int mid = start + (end-start)/2;
+
+    while(start<=end){
+        if(arr[mid]==key){
+            return mid;
+        }
+        if(arr[mid]<key){
+            start=mid+1;
+        }
+        else{
+            end = mid -1;
+        }
+        return -1;
+    }
+}
+int findPosition(int arr[],int size,int k){
+    int Pivot = getPivot(arr,size);
+    if(k>=arr[Pivot] && k<=arr[size-1]){
+        return BinarySearch(arr,Pivot,size-1,k);
+
+    }
+    else{
+        return BinarySearch(arr,0,Pivot,k);
+    }
+}
+
+int main(){
+    int arr[] = {7,9,1,2,3};
+    int size = sizeof(arr)/sizeof(int);
+
+    int target = 2;
+
+    cout<<findPosition(arr,size,target);
+
+    
+}
